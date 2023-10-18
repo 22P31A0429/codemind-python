17fb30d717faee99bde5830a6ed6aee168d7a204
PRIYANKA NALLAMILLI <121654292+22P31A0429@users.noreply.github.com>
2023-10-18 19:59:35 +0530

    def large_digit(n):     s=-1     while n>0:         t=n%10         if t>s:             s=t         n//=10     return s n=int(input()) result=large_digit(n) print(result)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,6 +1,26 @@
+def large_digit(n):         
+    s=-1                    
+    while n>0:                       
+        t=n%10
+        if t>s:
+            s=t
+        n//=10
+    return s
 n=int(input())
-a=str(n)
-list=[]
-for i in a:
-    list.append(i)
-print(max(list))
+result=large_digit(n)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
